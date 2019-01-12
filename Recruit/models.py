663476ac5ccbf19830e53d3224058154b527256e
Manzala <andrei.manzala@devpadawan.ro>
2019-01-12 11:10:48 +0200

    first draft of models

--- Recruit/models.py
+++ Recruit/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+__all__ = ['Recruiter','CV','Angajat','Anunt']
+
+
+class Recruiter(models.Model):
+    user = models.OneToOneField(User, related_name="recruiter_profile",  on_delete=models.CASCADE)
+    company = models.CharField(max_length=30)
+
+class Anunt(models.Model):
+    titlu = models.CharField(max_length=100)
+    descriere = models.CharField(max_length=1000)
+    created_by = models.ForeignKey(Recruiter, related_name="anunturi", on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CV(models.Model):
+    nume = models.CharField(max_length=40)
+    anunturi = models.ManyToManyField(Anunt,related_name="aplicatii")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Angajat(models.Model):
+    cvs = models.ForeignKey(CV, related_name="angajat", on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, related_name="angajat_profile",  on_delete=models.CASCADE)
+
