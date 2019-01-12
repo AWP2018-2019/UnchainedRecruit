a5ccc06ed2f9112f9d973e6574e9f448ae6e1115
K <nicuandreic@gmail.com>
2019-01-12 16:33:30 +0200

    anunturi

--- Recruit/forms.py
+++ Recruit/forms.py
@@ -9,8 +9,21 @@ class RecuiterForm(forms.ModelForm):
         model = models.Recruiter
 
 class AngajatForm(forms.ModelForm):
+'''class AnuntForm(forms.ModelForm):
+    class Meta:
+        model = models.Anunt
+        fields = ['text']'''
+
+class AnuntForm(forms.ModelForm):
+    class Meta:
+        model = models.Anunt
+        fields = ['descriere','titlu']
+
+'''class UserProfileForm(forms.ModelForm):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     e_mail = forms.CharField(max_length=100)
     class Meta:
         model = models.Angajat
+        model =models.UserProfile
+        exclude = ['user', 'friend_requests', 'friends']'''
--- Recruit/urls.py
+++ Recruit/urls.py
@@ -9,6 +9,11 @@ from .views import (
 
 urlpatterns = [ 
     path('',Index.as_view(), name='home'),
+    path(r'^anunt/<int:pk>/detail', AnuntView.as_view(), name='anunt'),
+    path(r'^anunt/<int:pk>/delete', AnuntDeleteView.as_view(), name='anunt_delete'),
+    path(r'^anunt/<int:pk>/edit', AnuntEditView.as_view(), name='anunt_edit'),
+   # path(r'^anunt/<int:pk>/aplica', AplicaAnunt.as_view(), name='aplica_anunt'),
+    path(r'^anunt/create', AnuntCreateView.as_view(), name='anunt_create'),
     path('anunt/<int:id>', Show.as_view(), name='anunt_detail'),
     path('register', Register.as_view(), name='register_path'),
     path('login', Login.as_view(), name='login_path'),
